39a5ada15116f537e35733d953a8af114e27841c
ken <kuanying@tradeserv.com>
2013-11-26 18:31:38 +0800

    limit phrase count to 1000

--- src/org/wen_input/WenPhraseTable.java
+++ src/org/wen_input/WenPhraseTable.java
@@ -33,29 +33,37 @@ public class WenPhraseTable {
                 new StringReader(data));
         try { 
             String line = reader.readLine();
-            while(line != null) {
+            int count=0;
+            while(line != null && count < 1000) {
                 final String[] values = line.split("\\s+");
-                if(values.length < 3) continue;
-                int freq = 0; 
-                try {
-                    freq = Integer.valueOf(values[1]);
-                } catch(NumberFormatException e) {
-                    e.printStackTrace();
-                }
-                final WenPhrase phrase = new WenPhrase(
-                    values[0], 
-                    freq, 
-                    Arrays.copyOfRange(values, 2, values.length));
+                if(values.length >= 3) {
+                    int freq = 0; 
+                    try {
+                        freq = Integer.valueOf(values[1]);
+                    } catch(NumberFormatException e) {
+                        e.printStackTrace();
+                    }
+                    final WenPhrase phrase = new WenPhrase(
+                        values[0], 
+                        freq, 
+                        Arrays.copyOfRange(values, 2, values.length));
+    
+                    if(phrase.value.length() <= 2) {
 System.out.println(phrase);
-                final String key = String.valueOf(values[0].charAt(0));
-                if(table.containsKey(key)) {
-                    final List<WenPhrase> phrases = table.get(key);
-                    phrases.add(phrase);
-                    Collections.sort(phrases);
-                } else {
-                    final List<WenPhrase> list = new ArrayList<WenPhrase>();
-                    list.add(phrase);
-                    table.put(key, list);
+                        final String key = String.valueOf(values[0].charAt(0));
+                        if(table.containsKey(key)) {
+                            final List<WenPhrase> phrases = table.get(key);
+                            if(! phrases.contains(phrase)) {
+                                phrases.add(phrase);
+                                Collections.sort(phrases);
+                            }
+                        } else {
+                            final List<WenPhrase> list = new ArrayList<WenPhrase>();
+                            list.add(phrase);
+                            table.put(key, list);
+                        }
+                        count++;
+                    }
                 }
                 line = reader.readLine();
             }
