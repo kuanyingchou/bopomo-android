9e6742427046d8ac29cef36d2b87d26dc378977d
ken <kuanying@tradeserv.com>
2013-11-26 18:32:45 +0800

    change toString to return value; add equlas() and hashCode()

--- src/org/wen_input/WenPhrase.java
+++ src/org/wen_input/WenPhrase.java
@@ -17,7 +17,33 @@ public class WenPhrase implements Comparable<WenPhrase> {
     }
     
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((value == null) ? 0 : value.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        WenPhrase other = (WenPhrase) obj;
+        if (value == null) {
+            if (other.value != null)
+                return false;
+        } else if (!value.equals(other.value))
+            return false;
+        return true;
+    }
+
+    @Override
     public String toString() {
-        return value+" ("+frequency+") "+WenUtil.join(key, ", ", "( ", ")");
+        //return value+" ("+frequency+") "+WenUtil.join(key, ", ", "( ", ")");
+        return value;
     }
 }
