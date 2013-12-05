c736c4b1e4b0613c94973cb739f296757bdb74f7
ken <kuanyingchou@gmail.com>
2013-12-05 10:25:40 +0800

    update comments

--- freq.sh
+++ freq.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Google <term> and get frequency from "About ###,### results"
+
+# How to convert from Simplified Chinese to Traditional Chinese?
+#   Use cconv:
+#   $ cconv -f UTF8-CN -t UTF8-HK jian.txt -o fan.txt
+
 input=phone.cin.chars.cconv.uniq
 output=freq.out
 
@@ -10,6 +16,13 @@ fi
 function get_count() {
   term=$1
   count=`curl -A "Mozilla/4.0" 2> /dev/null "http://www.google.com/search?q=%22$term%22" | grep -o 'About [0-9,]* results' | cut -d' ' -f2 | tr -d ','`
+
+  ## or use wget:
+  # wget \
+  # --user-agent Mozilla \  # so that Google thinks we are Mozilla
+  # --output-document=-  \  # print to stdout
+  # www.google.com/search?q=<term> 
+
   echo $count
 }
 
